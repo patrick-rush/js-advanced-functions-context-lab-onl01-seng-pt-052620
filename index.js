0405d752f1faee5e9710f6102cdfff3321b17485
patrick-rush <patrick.a.rush@gmail.com>
2020-12-20 18:35:19 -0500

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,75 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(array) {
+    const [firstName, familyName, title, payPerHour] = array; 
+    const timeInEvents = [];
+    const timeOutEvents = [];
+    const employee = {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: timeInEvents,
+        timeOutEvents: timeOutEvents
+    }
+    return employee;
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(array => {
+        return createEmployeeRecord(array);
+    })
+}
+
+function createTimeInEvent(dateStamp) {
+    const hour = dateStamp.split(" ")[1];
+    const date = dateStamp.split(" ")[0];
+    const record = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeInEvents.push(record);
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    const hour = dateStamp.split(" ")[1];
+    const date = dateStamp.split(" ")[0];
+    const record = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeOutEvents.push(record);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    function isDate(element) {
+        return (element.date === date);
+    };
+    const timeIn = this.timeInEvents.find(isDate);
+    const timeOut = this.timeOutEvents.find(isDate);
+    const hoursWorked = (timeOut.hour - timeIn.hour) / 100;
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    const wage = this.payPerHour;
+    return hoursWorked * wage;
+}
+
+function findEmployeeByFirstName(array, firstName) {
+    const employee = array.find(employee => employee.firstName === firstName);
+    return employee;
+}
+
+function calculatePayroll(array) {
+    const payroll = array.map(employee => allWagesFor.call(employee));
+    const payrollTotal = payroll.reduce((accum, current) => accum + current);
+    return payrollTotal;
+}   
